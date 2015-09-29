d5bc03f30d45654669c83f1dfed2415e04c58eee
ellatronic <ellachiangg@gmail.com>
2015-09-28 21:27:05 -0700

    hooked up API, displays city and current conditions

--- js/getweather.js
+++ js/getweather.js
@@ -1,5 +1,25 @@
 $(document).ready(function(){
+	var searchZip = '';
 	//get weather data
+	function getWeather(zipCode) {
+			$.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us')
+				.done(function(data) {
+					var name = data.name;
+					$('#cityName').text(name);
+					console.log(name);
+					var main = data.weather[0].main;
+					$('#forecast').text(main);
+					console.log(main);
+					console.log(data);
+			})
+	}
 
-	//call function
+	//call function using zip code
+	function search() {
+		searchZip = $('#zipcode').val();
+		getWeather(searchZip);
+	}
+	$('#button').click(search);
+
+	
 })
