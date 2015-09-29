8d0f28a6f1bb40cca17d3425c66e5972f2558690
ellatronic <ellachiangg@gmail.com>
2015-09-29 12:27:58 -0700

    added weather conditions description

--- js/getweather.js
+++ js/getweather.js
@@ -2,13 +2,16 @@ $(document).ready(function(){
 	var searchZip = '';
 	//get weather data
 	function getWeather(zipCode) {
-			$.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us')
+			$.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&APPID=ded17c04cecada392fc437e96cea4205')
 				.done(function(data) {
 					var name = data.name;
 					$('#cityName').text(name);
 					console.log(name);
 					var main = data.weather[0].main;
 					$('#forecast').text(main);
+					var description = data.weather[0].description;
+					$('#condition-description').text(description);
+					console.log(description);
 					console.log(main);
 					console.log(data);
 			})
