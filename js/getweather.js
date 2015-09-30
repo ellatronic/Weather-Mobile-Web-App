796bfa2dfc6c67561382e01476386fb69436b8cb
ellatronic <ellachiangg@gmail.com>
2015-09-30 16:59:46 -0700

    added current temperature, added 7 day forecast to js

--- js/getweather.js
+++ js/getweather.js
@@ -2,19 +2,27 @@ $(document).ready(function(){
 	var searchZip = '';
 	//get weather data
 	function getWeather(zipCode) {
-			$.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&APPID=ded17c04cecada392fc437e96cea4205')
+			//get current weather condiditons
+			$.get('http://api.openweathermap.org/data/2.5/weather?zip=' + zipCode + ',us&units=imperial&APPID=ded17c04cecada392fc437e96cea4205')
 				.done(function(data) {
 					var name = data.name;
 					$('#cityName').text(name);
-					console.log(name);
 					var main = data.weather[0].main;
 					$('#forecast').text(main);
-					var description = data.weather[0].description;
-					$('#condition-description').text(description);
-					console.log(description);
-					console.log(main);
-					console.log(data);
+					var temp = data.main.temp;
+					$('#temp').text(temp);
 			})
+			//get 7 day forecast
+			$.get('http://api.openweathermap.org/data/2.5/forecast/daily?q=' + zipCode + ',us&cnt=7&units=imperial&APPID=ded17c04cecada392fc437e96cea4205')
+				.done(function(forecastData) {
+					for (i = 0; i < 7; i++) {
+						var dayTemp = forecastData.list[i].temp.day;
+						console.log("Forecast Data for Day " + i + " " + dayTemp);
+					}
+					var day0Temp = forecastData.list[0].temp.day;
+					console.log(day0Temp);
+					console.log(forecastData);
+				})
 	}
 
 	//call function using zip code
